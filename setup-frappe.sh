e70428e7b32c1eb74a376ab5efe195d2790ffc5c
vishalseshagiri <vishaljsesh@gmail.com>
2017-07-24 08:52:16 +0000

    Port forwarding enabled+start-container.sh+setup.sh

--- setup-frappe.sh
+++
@@ -1,7 +0,0 @@
-db_ip=`getent hosts mariadb | awk '{ print $1 }'`
-bench set-mariadb-host $db_ip 
-bench new-site site1
-bench get-app erpnext https://github.com/frappe/erpnext
-bench --site site1 install-app erpnext
-bench start
-
