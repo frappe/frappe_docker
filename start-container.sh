5e84e477ac8f2b44d21024ff7f16ac082ded3904
vishalseshagiri <vishaljsesh@gmail.com>
2017-07-24 15:19:32 +0530

    start-container became build-container, added new start-container and updated README.md

--- start-container.sh
+++ start-container.sh
@@ -1,11 +1,3 @@
-echo "Enter a password for your database "
-read DB_PASS
-
-echo 'export DB_PASS='$DB_PASS >> ~/.bashrc
-source ~/.bashrc
-
-docker-compose up -d
 app_id=`docker ps | grep docker_frappe | awk {'print $1'}`
 
-docker exec -it $app_id bash -c './setup.sh; exec "${SHELL:-sh}"'
-
+docker exec -it $app_id bash -c 'cd /home/frappe/frappe-bench && su frappe; exec "${SHELL:-sh}"'
