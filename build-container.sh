165d66259b640d405d893d7feb745fba0be0ace5
vishalseshagiri <vishaljsesh@gmail.com>
2017-07-24 16:19:12 +0530

    Update build-container.sh
    Former-commit-id: 65898b6a75ec604e8001f6e69148d681f8df0127

--- build-container.sh
+++ build-container.sh
@@ -5,6 +5,3 @@ echo 'export DB_PASS='$DB_PASS >> ~/.bashrc
 source ~/.bashrc
 
 docker-compose up -d
-app_id=`docker ps | grep docker_frappe | awk {'print $1'}`
-
-docker exec -it $app_id bash -c './setup.sh; exec "${SHELL:-sh}"'
