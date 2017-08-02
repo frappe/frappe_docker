fa2b54c67c79b910a97768c959f13d77b7fa3dc6
vishalseshagiri <vishaljsesh@gmail.com>
2017-08-02 20:10:20 +0530

    amended commit

--- test.py
+++ test.py
@@ -1,10 +1,31 @@
-import subprocess,requests,time
+import subprocess, requests, datetime, _thread, time, os, signal
+start_time = datetime.datetime.now().time()
+r = None
+e = None
+bench_start = 'docker exec -i frappe bash -c "bench start"'
+process = subprocess.Popen(bench_start, shell=True)
+
+def print_out(val,delay):
+	while 1:
+		time.sleep(delay)
+		if val == 1:
+			result,error = process.communicate()
+			print(result)
+		elif val == 2:
+			try:
+				r = requests.get("http://site1.local:8000")
+				print(r)
+			except requests.exceptions.ConnectionError as e:
+				print(e)
+
+_thread.start_new_thread(print_out, (1, 1))
+#result, error = process.communicate()
+_thread.start_new_thread(print_out, (2, 1))
 
 time.sleep(45)
 
-try:
-    r = requests.get("site1.local:8000")
-    assert '<title> Login </title>' in r.content, "Login page failed to load"
-except Exception as e:
-    traceback.print_exc(e)
-    sys.exit(3)
+os.killpg(os.getpgid(process.pid), signal.SIGTERM) # Kill bench start
+
+print(r.content)
+
+assert '<title> Login </title>' in r.content, "Login page failed to load"
