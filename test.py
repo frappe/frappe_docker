fbf13bbc2ef252d0f0bef9df1da240f6966aba91
vishalseshagiri <vishaljsesh@gmail.com>
2017-08-02 16:46:50 +0530

    Added bench init , start and python tests to curl the site
    Former-commit-id: 3f148638fb596fc922bd24e045eb1a568443ffaf

---
+++ test.py
@@ -0,0 +1,10 @@
+import subprocess,requests,time
+
+time.wait(45)
+
+try:
+    r = requests.get("site1.local:8000")
+    assert '<title> Login </title>' in r.content, "Login page failed to load"
+except Exception as e:
+    traceback.print_exc(e)
+    sys.exit(3)
