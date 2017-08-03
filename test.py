662aa6562fa0d09693f5892a6c3f8d22c03c3682
vishalseshagiri <vishaljsesh@gmail.com>
2017-08-03 15:58:30 +0530

    global r
    Former-commit-id: 1f2e8eb0e84705e4a8150d07ae32e05ae1654d11

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ start_time = datetime.datetime.now().time()
 bench_start = 'docker exec -i frappe bash -c "bench start"'
 process = subprocess.Popen(bench_start, stdout=subprocess.PIPE , shell=True)
 
-r = None
 
 def print_out(val,delay):
 	while 1:
@@ -13,6 +12,7 @@ def print_out(val,delay):
 			print(result)
 		elif val == 2:
 			try:
+				global r
 				r = requests.get("http://site1.local:8000")
 				print(r)
 			except requests.exceptions.ConnectionError as e:
@@ -22,9 +22,8 @@ _thread.start_new_thread(print_out, (1, 1))
 #result, error = process.communicate()
 _thread.start_new_thread(print_out, (2, 1))
 
-time.sleep(45)
+time.sleep(30)
 
 process.kill()
 
 assert '<title> Login </title>' in str(r.content)
-#os.killpg(os.getpgid(process.pid), signal.SIGTERM) # Kill bench start
