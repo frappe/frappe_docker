58824ca35875e7f7c3acc92b696ec7011df88d6e
Mendel Greenberg <chabad360@gmail.com>
2019-03-04 00:03:18 -0500

    trying this instead

--- test.sh
+++ test.sh
@@ -2,13 +2,14 @@
 
 ./dbench start > /tmp/bench.log &
 
-output=$(
-  while ! curl "http://localhost:8000"
-  do
-      { echo "Exit status of curl: $?"
-      } 1>&2
-      sleep 1
-  done
-)
+while ! [[ $i == 20 ]]
+do
+    output=$( curl "http://localhost:8000" )
+    { echo "Exit status of curl: $?"
+    } 1>&2
+    sleep 2
+    i=$((i + 1))
+done
+
 
 echo "${output}" | grep '<title> Login </title>' || exit 1
