a3bdc3c8f90f32c6ef6af7d5b532ee40288a930f
Revant Nandgaonkar <revant.one@gmail.com>
2020-08-07 15:28:15 +0530

    fix: remove nunjucks from nginx image preparation

--- build/frappe-nginx/build.sh
+++ build/frappe-nginx/build.sh
@@ -8,18 +8,10 @@ function nodeCleanUp() {
 cd /home/frappe/frappe-bench/apps/frappe
 yarn
 yarn run production
-# Remove this after python container starts using node command
-yarn add nunjucks -D
 
 if [[ "$GIT_BRANCH" =~ ^(version-12|version-11)$ ]]; then
     nodeCleanUp
 else
-    node generate_standard_style_css.js \
-            frappe/website/doctype/website_theme/website_theme_template.scss > \
-            /home/frappe/standard_templates_string
-    node generate_bootstrap_theme.js \
-            /home/frappe/frappe-bench/sites/assets/css/standard_style.css \
-            "$(cat /home/frappe/standard_templates_string)"
     nodeCleanUp
     # remove this when frappe framework moves this to dependencies from devDependencies
     yarn add node-sass
