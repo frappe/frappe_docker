a4c814f9940a561fa8ff41fd4ee68d9086c76a78
Revant Nandgaonkar <revant.one@gmail.com>
2020-09-15 23:18:28 +0530

    fix: health check
    use shell script instead of python

---
+++ build/common/worker/healthcheck.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set DB Host and port
+export DB_HOST=`cat ./common_site_config.json | awk '/db_host/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n'`
+export DB_PORT=`cat ./common_site_config.json | awk '/db_port/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n'`
+if [[ -z "$DB_PORT" ]]; then
+    export DB_PORT=3306
+fi
+
+# Set REDIS host:port
+export REDIS_CACHE=`cat ./common_site_config.json | awk '/redis_cache/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'`
+export REDIS_QUEUE=`cat ./common_site_config.json | awk '/redis_queue/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'`
+export REDIS_SOCKETIO=`cat ./common_site_config.json | awk '/redis_socketio/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'`
+
+echo "Check $DB_HOST:$DB_PORT"
+wait-for-it.sh $DB_HOST:$DB_PORT -t 1
+echo "Check $REDIS_CACHE"
+wait-for-it.sh $REDIS_CACHE -t 1
+echo "Check $REDIS_QUEUE"
+wait-for-it.sh $REDIS_QUEUE -t 1
+echo "Check $REDIS_SOCKETIO"
+wait-for-it.sh $REDIS_SOCKETIO -t 1
+
+if [[ "$1" = "-p" ]] || [[ "$1" = "--ping-service" ]]; then
+    echo "Check $2"
+    wait-for-it.sh $2 -t 1
+fi
