8d2c139fc62bdcf4edde06cc9a21b931e82b567f
Revant Nandgaonkar <revant.one@gmail.com>
2021-01-07 19:37:46 +0530

    ci: use docker buildx in travis

--- build/erpnext-nginx/install_app.sh
+++ build/erpnext-nginx/install_app.sh
@@ -18,7 +18,6 @@ git clone --depth 1 ${APP_REPO} ${BRANCH} ${APP_NAME}
 cd /home/frappe/frappe-bench/apps/frappe
 yarn
 yarn production --app ${APP_NAME}
-rm -fr node_modules
 yarn install --production=true
 yarn add node-sass
 
