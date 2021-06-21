3d67150229abc3b4107ebb979fe4ca7f90007764
pratikbalar <pratikbalar123@gmail.com>
2021-06-21 22:56:05 +0530

    refactor: add function for redis
    - Style it as per google shell  developing guide

--- build/common/worker/healthcheck.sh
+++ build/common/worker/healthcheck.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 set -ea
 
+function getRedisUrl() {
+  cat ${1} | grep $2 | awk -v word=$2 '$word { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'
+}
+
 COMMON_SITE_CONFIG_JSON='/home/frappe/frappe-bench/sites/common_site_config.json'
 
 # Set DB Host and port
-DB_HOST=`cat $COMMON_SITE_CONFIG_JSON | awk '/db_host/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n'`
-DB_PORT=`cat $COMMON_SITE_CONFIG_JSON | awk '/db_port/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n'`
+DB_HOST=$(cat $COMMON_SITE_CONFIG_JSON | awk '/db_host/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n')
+DB_PORT=$(cat $COMMON_SITE_CONFIG_JSON | awk '/db_port/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n')
 if [[ -z "$DB_PORT" ]]; then
-    DB_PORT=3306
+  DB_PORT=3306
 fi
 
 # Set REDIS host:port
-REDIS_CACHE=`cat $COMMON_SITE_CONFIG_JSON | awk '/redis_cache/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'`
+REDIS_CACHE=$(getRedisUrl "$COMMON_SITE_CONFIG_JSON" "redis_cache")
 if [[ "$REDIS_CACHE" == *"/"* ]]; then
-  REDIS_CACHE=`echo $REDIS_CACHE | cut -f1 -d"/"`
+  REDIS_CACHE=$(echo $REDIS_CACHE | cut -f1 -d"/")
 fi
 
-REDIS_QUEUE=`cat $COMMON_SITE_CONFIG_JSON | awk '/redis_queue/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'`
+REDIS_QUEUE=$(getRedisUrl "$COMMON_SITE_CONFIG_JSON" "redis_queue")
 if [[ "$REDIS_QUEUE" == *"/"* ]]; then
-  REDIS_QUEUE=`echo $REDIS_QUEUE | cut -f1 -d"/"`
+  REDIS_QUEUE=$(echo $REDIS_QUEUE | cut -f1 -d"/")
 fi
 
-REDIS_SOCKETIO=`cat $COMMON_SITE_CONFIG_JSON | awk '/redis_socketio/ { gsub(/[",]/,"",$2); print $2}' | tr -d '\n' | sed 's|redis://||g'`
+REDIS_SOCKETIO=$(getRedisUrl "$COMMON_SITE_CONFIG_JSON" "redis_socketio")
 if [[ "$REDIS_SOCKETIO" == *"/"* ]]; then
-  REDIS_SOCKETIO=`echo $REDIS_SOCKETIO | cut -f1 -d"/"`
+  REDIS_SOCKETIO=$(echo $REDIS_SOCKETIO | cut -f1 -d"/")
 fi
 
 echo "Check $DB_HOST:$DB_PORT"
-wait-for-it $DB_HOST:$DB_PORT -t 1
+wait-for-it "$DB_HOST:$DB_PORT" -t 1
 echo "Check $REDIS_CACHE"
-wait-for-it $REDIS_CACHE -t 1
+wait-for-it "$REDIS_CACHE" -t 1
 echo "Check $REDIS_QUEUE"
-wait-for-it $REDIS_QUEUE -t 1
+wait-for-it "$REDIS_QUEUE" -t 1
 echo "Check $REDIS_SOCKETIO"
-wait-for-it $REDIS_SOCKETIO -t 1
+wait-for-it "$REDIS_SOCKETIO" -t 1
 
 if [[ "$1" = "-p" ]] || [[ "$1" = "--ping-service" ]]; then
-    echo "Check $2"
-    wait-for-it $2 -t 1
+  echo "Check $2"
+  wait-for-it "$2" -t 1
 fi
