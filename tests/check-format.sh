f0399fa389469c2b22705f2c37237740722256b6
Revant Nandgaonkar <revant.one@gmail.com>
2021-06-24 17:43:25 +0530

    ci: fix check format script
    [skip travis]

--- tests/check-format.sh
+++ tests/check-format.sh
@@ -2,10 +2,6 @@
 
 echo "Checking bash scripts with shellcheck" >&2
 
-shellcheck --check-sourced --severity=style --color=always --exclude=SC2164,SC2086,SC2012,SC2016 \
-  build/common/worker/docker-entrypoint.sh \
-  build/common/worker/healthcheck.sh \
-  build/common/worker/install_app.sh \
-  build/erpnext-nginx/install_app.sh \
-  build/frappe-nginx/docker-entrypoint.sh \
-  build/frappe-socketio/docker-entrypoint.sh
+while IFS= read -r shellfile; do
+  shellcheck --check-sourced --severity=style --color=always --exclude=SC2164,SC2086,SC2012,SC2016 ${shellfile}
+done < <(find ./build -name "*.sh")
