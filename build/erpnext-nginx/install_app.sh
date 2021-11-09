7c157aa487c52045974bfeb7f00b4110d1748372
Lev <mail@vrslev.com>
2021-11-10 02:13:46 +0300

    refactor: Dockerfiles (#555)
    * refactor(frappe-worker): Dockerfile
    
    * fix(frappe-worker): Cloning frappe repo
    
    * refactor(frappe-nginx): Dockerfile
    
    * refactor(erpnext-nginx): Dockerfile
    
    * chore(frappe-worker): Add space in Dockerfile
    
    * refactor(bench): Dockerfile
    
    * refactor(frappe-socketio): Dockerfile
    
    * ci(Test): Add `workflow_dispatch` trigger
    
    * fix(bench): Uncomment missing package
    
    * fix(bench): Replace legacy `libmariadbclient-dev` with `libmariadb-dev`
    
    * refactor(erpnext-nginx): Install script
    
    - Remove repetitive code
    - Don't explicitly install production deps (they are installed via plain `yarn` with dev deps)
    - Don't write to lockfiles
    
    * fix(erpnext-nginx): use -e flag, fix installation
    
    * fix(erpnext-nginx): Don't install unused wget
    
    * fix(frappe-nginx): Get newest certificates
    
    * fix: Install ca-certificates on erpnext-nginx instead of frappe-nginx
    
    * trigger ci
    
    * ci(test): Fix trigger duplicate
    
    * chore: Fix linting

--- build/erpnext-nginx/install_app.sh
+++ build/erpnext-nginx/install_app.sh
@@ -9,40 +9,32 @@ FRAPPE_BRANCH=${4}
 
 [ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
 
-mkdir -p /home/frappe/frappe-bench/sites/assets
+mkdir -p /home/frappe/frappe-bench
 cd /home/frappe/frappe-bench
-echo -ne "frappe\n${APP_NAME}" >/home/frappe/frappe-bench/sites/apps.txt
+mkdir -p apps "sites/assets/${APP_NAME}"
+echo -ne "frappe\n${APP_NAME}" >sites/apps.txt
 
-mkdir -p apps
-cd apps
-git clone --depth 1 https://github.com/frappe/frappe -b "${FRAPPE_BRANCH}"
+git clone --depth 1 -b "${FRAPPE_BRANCH}" https://github.com/frappe/frappe apps/frappe
 # shellcheck disable=SC2086
-git clone --depth 1 "${APP_REPO}" ${BRANCH} "${APP_NAME}"
+git clone --depth 1 ${BRANCH} ${APP_REPO} apps/${APP_NAME}
 
 echo "Install frappe NodeJS dependencies . . ."
-cd /home/frappe/frappe-bench/apps/frappe
-yarn
-echo "Install ${APP_NAME} NodeJS dependencies . . ."
-cd "/home/frappe/frappe-bench/apps/${APP_NAME}"
-yarn
-echo "Build browser assets . . ."
-cd /home/frappe/frappe-bench/apps/frappe
-yarn production --app "${APP_NAME}"
-echo "Install frappe NodeJS production dependencies . . ."
-cd /home/frappe/frappe-bench/apps/frappe
-yarn install --production=true
-echo "Install ${APP_NAME} NodeJS production dependencies . . ."
-cd "/home/frappe/frappe-bench/apps/${APP_NAME}"
-yarn install --production=true
+cd apps/frappe
+yarn --pure-lockfile
 
+echo "Install ${APP_NAME} NodeJS dependencies . . ."
+yarn --pure-lockfile --cwd "../${APP_NAME}"
+
+echo "Build ${APP_NAME} assets . . ."
+yarn production --app "${APP_NAME}"
+
+cd /home/frappe/frappe-bench
 # shellcheck disable=SC2086
-mkdir -p /home/frappe/frappe-bench/sites/assets/${APP_NAME}
-# shellcheck disable=SC2086
-cp -R /home/frappe/frappe-bench/apps/${APP_NAME}/${APP_NAME}/public/* /home/frappe/frappe-bench/sites/assets/${APP_NAME}
+cp -R apps/${APP_NAME}/${APP_NAME}/public/* sites/assets/${APP_NAME}
 
 # Add frappe and all the apps available under in frappe-bench here
 echo "rsync -a --delete /var/www/html/assets/frappe /assets" >/rsync
 echo "rsync -a --delete /var/www/html/assets/${APP_NAME} /assets" >>/rsync
 chmod +x /rsync
 
-rm /home/frappe/frappe-bench/sites/apps.txt
+rm sites/apps.txt
