f74ff3cf627863826eba00c5c7568e0bebe6648e
Revant Nandgaonkar <revant.one@gmail.com>
2023-08-29 09:33:54 +0530

    ci: changes for version 15

--- development/installer.py
+++ development/installer.py
@@ -72,8 +72,8 @@ def get_args_parser():
         "--frappe-branch",
         action="store",
         type=str,
-        help="frappe repo to use, default: version-14",  # noqa: E501
-        default="version-14",
+        help="frappe repo to use, default: version-15",  # noqa: E501
+        default="version-15",
     )
     parser.add_argument(
         "-p",
