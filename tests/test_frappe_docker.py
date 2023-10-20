011166a41e9c975843746d03c4f1b91cc17a6f60
Revant Nandgaonkar <revant.one@gmail.com>
2023-10-20 17:58:54 +0530

    ci: fix test endpoint
    changed to erpnext.templates.pages.search_help.get_help_results_sections

--- tests/test_frappe_docker.py
+++ tests/test_frappe_docker.py
@@ -126,7 +126,7 @@ class TestErpnext:
         ("url", "callback"),
         (
             (
-                "/api/method/erpnext.templates.pages.product_search.get_product_list",
+                "/api/method/erpnext.templates.pages.search_help.get_help_results_sections?text=help",
                 api_cb,
             ),
             ("/assets/erpnext/js/setup_wizard.js", assets_cb),
