bde35bd1f33f0949035fbf8f15709b0de5ed5867
Revant Nandgaonkar <revant.one@gmail.com>
2023-10-20 17:31:05 +0530

    ci: fix v15 build

--- .github/scripts/get_latest_tags.py
+++ .github/scripts/get_latest_tags.py
@@ -9,7 +9,7 @@ import sys
 from typing import Literal
 
 Repo = Literal["frappe", "erpnext"]
-MajorVersion = Literal["12", "13", "14", "develop"]
+MajorVersion = Literal["12", "13", "14", "15", "develop"]
 
 
 def get_latest_tag(repo: Repo, version: MajorVersion) -> str:
