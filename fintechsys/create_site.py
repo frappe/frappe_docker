fa3282f47b7e3892f8f78cebec12b4f951496764
fal7w <147312030+fal7w@users.noreply.github.com>
2023-12-01 20:48:41 +0300

    Update create_site.py

--- fintechsys/create_site.py
+++ fintechsys/create_site.py
@@ -16,7 +16,7 @@ def run_docker_command(command):
 
 if __name__ == "__main__":
 
-    docker_command = "docker compose --project-name erpnext-one exec backend \
+    docker_command = "docker compose --project-name erpnext-one exec -T backend \
   bench new-site --no-mariadb-socket --mariadb-root-password fintech2023 --install-app hrms --set-default \
            --install-app rule_management --install-app remittance_base --install-app remittance --install-app bulk_remittance --install-app remittance_stellar_integration \
            --install-app client_account_management  --install-app teller_for_erpnext --install-app teller_for_agent \
