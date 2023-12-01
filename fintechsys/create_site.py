508fa8684f1b4817583bac421ca170db1ac8c76a
fal7w <147312030+fal7w@users.noreply.github.com>
2023-12-01 16:48:41 +0300

    Update create_site.py

--- fintechsys/create_site.py
+++ fintechsys/create_site.py
@@ -17,15 +17,11 @@ def run_docker_command(command):
 if __name__ == "__main__":
     # Define your Docker Compose command
     docker_command = "docker compose --project-name erpnext-one exec backend \
-        bench new-site --no-mariadb-socket --mariadb-root-password fintech2023 \
-        --install-app hrms --set-default --install-app rule_management \
-        --install-app remittance_base --install-app remittance --install-app bulk_remittance \
-        --install-app remittance_stellar_integration --install-app client_account_management \
-        --install-app teller_for_erpnext --install-app teller_for_agent \
-        --install-app remittance_agent_service --install-app payment_management \
-        --install-app bank_services --install-app remittance_customize \
-        --install-app remittance_network_manager --install-app erpnext_theme \
-        --install-app remittance_website --admin-password fintech2023 agent.fintechsys.net"
+      bench new-site --no-mariadb-socket --mariadb-root-password fintech2023 --install-app hrms --set-default \
+           --install-app rule_management --install-app remittance_base --install-app remittance --install-app bulk_remittance --install-app remittance_stellar_integration \
+           --install-app client_account_management  --install-app teller_for_erpnext --install-app teller_for_agent \
+           --install-app remittance_agent_service --install-app payment_management --install-app bank_services --install-app remittance_customize \
+           --install-app remittance_network_manager --install-app erpnext_theme --install-app remittance_website --admin-password fintech2023  agent.fintechsys.net"
 
     # Call the function with your Docker Compose command
     run_docker_command(docker_command)
