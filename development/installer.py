beecd99686c534afc8971fbc95018c985ae5b050
0xD0M1M0 <76812428+0xD0M1M0@users.noreply.github.com>
2024-12-07 12:59:14 +0100

    fix: --no-mariadb-socket is DEPRECATED (#1525)
    --no-mariadb-socket is DEPRECATED; use --mariadb-user-host-login-scope='%' (wildcard) or --mariadb-user-host-login-scope=<myhostscope>, instead.

--- development/installer.py
+++ development/installer.py
@@ -209,7 +209,7 @@ def create_site_in_bench(args):
             "new-site",
             f"--db-host=mariadb",  # Should match the compose service name
             f"--db-type={args.db_type}",  # Add the selected database type
-            f"--no-mariadb-socket",
+            f"--mariadb-user-host-login-scope=%",
             f"--db-root-password=123",  # Replace with your MariaDB password
             f"--admin-password={args.admin_password}",
         ]
