3a5b15cf5d4a73bb7245612760bdab51df8f4cf0
bgodlin <37313677+bgodlin@users.noreply.github.com>
2025-02-21 14:06:55 +0100

    Update installer.py
    Revising the `installer.py` script. The script is facing an issue during execution as it required interactive input for the root username unless the username was provided as a flag https://github.com/frappe/frappe/blob/42f9e537d0de84bdcd540281d5f8149ea5d50eaa/frappe/installer.py#L157-L158. Resolving this by passing the username as a flag allows the script to proceed successfully.

--- development/installer.py
+++ development/installer.py
@@ -207,6 +207,7 @@ def create_site_in_bench(args):
         new_site_cmd = [
             "bench",
             "new-site",
+            f"--db-root-username=root",
             f"--db-host=mariadb",  # Should match the compose service name
             f"--db-type={args.db_type}",  # Add the selected database type
             f"--mariadb-user-host-login-scope=%",
@@ -222,6 +223,7 @@ def create_site_in_bench(args):
         new_site_cmd = [
             "bench",
             "new-site",
+            f"--db-root-username=root",
             f"--db-host=postgresql",  # Should match the compose service name
             f"--db-type={args.db_type}",  # Add the selected database type
             f"--db-root-password=123",  # Replace with your PostgreSQL password
