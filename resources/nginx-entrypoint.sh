77a9abb3bcf34314dd89afe35be01092e084165d
Hemant <heman4t@gmail.com>
2025-03-28 13:43:29 +0530

    chore: update Traefik to v3.3.4 and add MailHog and phpMyAdmin services

--- resources/nginx-entrypoint.sh
+++ resources/nginx-entrypoint.sh
@@ -1,44 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
-# Set variables that do not exist
-if [[ -z "$BACKEND" ]]; then
-  echo "BACKEND defaulting to 0.0.0.0:8000"
-  export BACKEND=0.0.0.0:8000
-fi
-if [[ -z "$SOCKETIO" ]]; then
-  echo "SOCKETIO defaulting to 0.0.0.0:9000"
-  export SOCKETIO=0.0.0.0:9000
-fi
-if [[ -z "$UPSTREAM_REAL_IP_ADDRESS" ]]; then
-  echo "UPSTREAM_REAL_IP_ADDRESS defaulting to 127.0.0.1"
-  export UPSTREAM_REAL_IP_ADDRESS=127.0.0.1
-fi
-if [[ -z "$UPSTREAM_REAL_IP_HEADER" ]]; then
-  echo "UPSTREAM_REAL_IP_HEADER defaulting to X-Forwarded-For"
-  export UPSTREAM_REAL_IP_HEADER=X-Forwarded-For
-fi
-if [[ -z "$UPSTREAM_REAL_IP_RECURSIVE" ]]; then
-  echo "UPSTREAM_REAL_IP_RECURSIVE defaulting to off"
-  export UPSTREAM_REAL_IP_RECURSIVE=off
-fi
-if [[ -z "$FRAPPE_SITE_NAME_HEADER" ]]; then
-  # shellcheck disable=SC2016
-  echo 'FRAPPE_SITE_NAME_HEADER defaulting to $host'
-  # shellcheck disable=SC2016
-  export FRAPPE_SITE_NAME_HEADER='$host'
-fi
+# Default values
+BACKEND=${BACKEND:-0.0.0.0:8000}
+SOCKETIO=${SOCKETIO:-0.0.0.0:9000}
+UPSTREAM_REAL_IP_ADDRESS=${UPSTREAM_REAL_IP_ADDRESS:-127.0.0.1}
+UPSTREAM_REAL_IP_HEADER=${UPSTREAM_REAL_IP_HEADER:-X-Forwarded-For}
+UPSTREAM_REAL_IP_RECURSIVE=${UPSTREAM_REAL_IP_RECURSIVE:-off}
+FRAPPE_SITE_NAME_HEADER=${FRAPPE_SITE_NAME_HEADER:-$host}
+PROXY_READ_TIMEOUT=${PROXY_READ_TIMEOUT:-120}
+CLIENT_MAX_BODY_SIZE=${CLIENT_MAX_BODY_SIZE:-50m}
 
-if [[ -z "$PROXY_READ_TIMEOUT" ]]; then
-  echo "PROXY_READ_TIMEOUT defaulting to 120"
-  export PROXY_READ_TIMEOUT=120
-fi
-
-if [[ -z "$CLIENT_MAX_BODY_SIZE" ]]; then
-  echo "CLIENT_MAX_BODY_SIZE defaulting to 50m"
-  export CLIENT_MAX_BODY_SIZE=50m
-fi
-
-# shellcheck disable=SC2016
+# Generate nginx configuration
 envsubst '${BACKEND}
   ${SOCKETIO}
   ${UPSTREAM_REAL_IP_ADDRESS}
@@ -46,7 +19,8 @@ envsubst '${BACKEND}
   ${UPSTREAM_REAL_IP_RECURSIVE}
   ${FRAPPE_SITE_NAME_HEADER}
   ${PROXY_READ_TIMEOUT}
-	${CLIENT_MAX_BODY_SIZE}' \
-  </templates/nginx/frappe.conf.template >/etc/nginx/conf.d/frappe.conf
+  ${CLIENT_MAX_BODY_SIZE}' \
+  < /templates/nginx/frappe.conf.template > /etc/nginx/conf.d/frappe.conf
 
-nginx -g 'daemon off;'
+# Start nginx
+exec nginx -g 'daemon off;'
