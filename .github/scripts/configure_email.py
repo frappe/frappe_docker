5e9e0b20922e314baed01e8f997a37e8f47091e0
Kwosh Vick <kwoshvick@gmail.com>
2026-05-21 15:49:28 +0300

    fix: move email setup script out of YAML to avoid heredoc parsing error
    Python heredoc at column 0 terminated the YAML literal block scalar.
    Replace with a separate script file that gets base64-encoded on the
    runner and decoded into python3 stdin on the server.

---
+++ .github/scripts/configure_email.py
@@ -0,0 +1,39 @@
+import frappe, os, sys
+
+site     = os.environ.get('SITE_NAME', '')
+login    = os.environ.get('MAIL_LOGIN', '')
+server   = os.environ.get('MAIL_SERVER', '')
+port     = int(os.environ.get('MAIL_PORT', '587'))
+use_tls  = int(os.environ.get('MAIL_USE_TLS', '1'))
+password = os.environ.get('MAIL_PASSWORD', '')
+sender   = os.environ.get('MAIL_DEFAULT_SENDER') or login
+
+if not (site and login and server):
+    sys.exit(0)
+
+frappe.init(site=site, sites_path='sites')
+frappe.connect()
+
+existing = frappe.db.get_value('Email Account', {'email_id': login}, 'name')
+if existing:
+    doc = frappe.get_doc('Email Account', existing)
+else:
+    doc = frappe.new_doc('Email Account')
+    doc.email_account_name = 'System Notifications'
+    doc.email_id = login
+
+doc.smtp_server = server
+doc.smtp_port = port
+doc.use_tls = use_tls
+doc.password = password
+doc.enable_outgoing = 1
+doc.default_outgoing = 1
+
+if existing:
+    doc.save(ignore_permissions=True)
+else:
+    doc.insert(ignore_permissions=True)
+
+frappe.db.commit()
+frappe.destroy()
+print('Email account configured as default outgoing.')
